0a906d384fa2c7b14726dc3b4ff85a1cb4a03e2f
matypucv <matias.gomez.a01@mail.pucv.cl>
2026-03-26 17:30:50 -0300

    0.0.2
    Reiniciar nivel
    Firework
    Ambos inputsystem funcionan

---
+++ Assets/Scenemanager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+public class Scenemanager : MonoBehaviour
+{
+    public void reloadcurrentscene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            reloadcurrentscene();
+        }
+    }
+
+}
